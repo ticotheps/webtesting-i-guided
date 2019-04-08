0f588bf653150709105583cbe499273d83cacb32
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-08 14:02:08 -0400

    Adds some structure and hierarchy to the calculator.spec.js file using the '--verbose' flag in package.json (added to the 'test' script) and using the 'describe();' statements in calculator.spec.js

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -21,43 +21,52 @@ const { add } = require('./calculator.js');
 //     expect(sum).toEqual(expected);
 // });
 
-// test case
-it('should add 2 numbers correctly', () => {
-    expect(add(2, 2)).toBe(4);
+describe('calculator.js', () => {
+
+    describe('calculator', () => {
+
+        function honest() {
+            return true;
+        }
+
+        test('an honest function should always tell the truth', () => {
+            expect(honest()).toBeTruthy();
+        });
+
+        describe('add()', () => {
+            // test case
+            it('should add 2 numbers correctly', () => {
+                expect(add(2, 2)).toBe(4);
+            });
+
+            // should return the passed value, if only one value provided
+            // 1 + undefined = undefined
+            it('should return the passed value, if only one value provided', () => {
+                expect(add(1)).toBe(1);
+                expect(add(99)).toBe(99);
+            });
+
+            // should calculate the sum of any number of arguments separated by a comma
+            it('should calculate the sum of any number of arguments separated by a comma', () => {
+                expect(add(1, 2, 3)).toBe(6);
+                expect(add(1, 2, 3, 4)).toBe(10);
+            });
+
+            // should handle an array of numbers
+            it('should handle an array of numbers', () => {
+                expect(add([1, 2, 3])).toBe(6);
+                expect(add([1, 2, 3, 4])).toBe(10);
+            });
+
+            // create some todos for yourself to show up in the console
+            // to help remember what other tests you'd like to try
+            it.todo('pass in a string');
+            it.todo('pass in null');
+            it.todo('pass in an array');
+        });
+    });
 });
 
-// should return the passed value, if only one value provided
-// 1 + undefined = undefined
-it('should return the passed value, if only one value provided', () => {
-    expect(add(1)).toBe(1);
-    expect(add(99)).toBe(99);
-});
-
-// should calculate the sum of any number of arguments separated by a comma
-it('should calculate the sum of any number of arguments separated by a comma', () => {
-    expect(add(1, 2, 3)).toBe(6);
-    expect(add(1, 2, 3, 4)).toBe(10);
-});
-
-// should handle an array of numbers
-it('should handle an array of numbers', () => {
-    expect(add([1, 2, 3])).toBe(6);
-    expect(add([1, 2, 3, 4])).toBe(10);
-});
-
-// create some todos for yourself to show up in the console
-// to help remember what other tests you'd like to try
-it.todo('pass in a string');
-it.todo('pass in null');
-it.todo('pass in an array');
-
-function honest() {
-    return true;
-}
-
-test('an honest function should always tell the truth', () => {
-    expect(honest()).toBeTruthy();
-});
 
 //-----------NOTES FROM VIDEO--------------
 
