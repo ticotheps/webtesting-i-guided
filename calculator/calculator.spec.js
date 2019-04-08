e5d4b5b6e4f7c100c4c9d77da44373a2d9c38db8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-08 12:44:22 -0400

    Creates a minimally-working 'add()' function in the calculator.js file and creates a corresponding test suite in the calculator.spec.js file

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,23 @@
 const { add } = require('./calculator.js');
 
-// test away!
+describe('math.js', () => {
+    describe('add()', () => {
+
+        test('adds two numbers correctly', () => {
+            expect(add(2, 2)).toBe(4); 
+            expect(add(1, 3)).toBe(4); 
+            expect(add(-1, 3)).toBe(2); 
+        });
+
+        // it('add() should return null when arguments are not numbers', () => {
+        //     // expect(add()).toBeNull();
+        //     // expect(add(1)).toBeNull();
+        //     // expect(add(null, 1)).toBeNull();
+        //     // expect(add(undefined, 1)).toBeNull();
+        //     // expect(add(3, '4')).toBeNull();
+        //     // expect(add({}, '4')).toBeNull();
+        //     // expect(add([], '4')).toBeNull();
+        //     // expect(add(NaN, '4')).toBeNull();
+        // });
+    });
+});
